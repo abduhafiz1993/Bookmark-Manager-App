75c83397fe16184a5eeafefd7f507487dadcd361
Abdulhafiz Muhammed <muhammedabdulhafiz1993@gmail.com>
2025-07-21 00:02:35 -0700

    Delete fuctionality is added

--- bookmark-manager/src/components/BookmarkList.jsx
+++ bookmark-manager/src/components/BookmarkList.jsx
@@ -1,4 +1,4 @@
-function BookmarkList({ bookmarks }) {
+function BookmarkList({ bookmarks, onDelete }) {
       if (bookmarks.length === 0) {
     return <p className="text-gray-600">No bookmarks yet.</p>;
   }
@@ -6,9 +6,17 @@ function BookmarkList({ bookmarks }) {
      <div className="space-y-4">
       {bookmarks.map((bookmark) => (
         <div key={bookmark.id} className="bg-gray-100 p-4 rounded shadow">
+          <div className="flex justify-between items-center">
           <a href={bookmark.url} target="_blank" rel="noopener noreferrer" className="text-blue-600 font-semibold hover:underline">
             {bookmark.title}
           </a>
+            <button
+              onClick={() => onDelete(bookmark.id)}
+              className="bg-red-500 text-white text-sm px-3 py-1 rounded hover:bg-red-600"
+            >
+              Delete
+            </button>
+            </div>
           {bookmark.note && <p className="text-sm text-gray-700 mt-1">{bookmark.note}</p>}
         </div>
       ))}
--- bookmark-manager/src/pages/Home.jsx
+++ bookmark-manager/src/pages/Home.jsx
@@ -4,15 +4,21 @@ import BookmarkList from "../components/BookmarkList";
 
 function Home() {
 const [bookmarks, setBookmarks] = useState([]);
+
 const addBookmark = (bookmark) => {
     setBookmarks([bookmark ,...bookmarks]);
-};    
+};
+
+  const deleteBookmark = (id) => {
+    const updated = bookmarks.filter((b) => b.id !== id);
+    setBookmarks(updated);
+  };
   return (
     <div>
       <h2 className="text-2xl font-bold mb-4">Your Bookmarks</h2>
       {/* BookmarkForm and BookmarkList will go here */}
       < BookmarkForm onAdd = {addBookmark} />
-      <BookmarkList bookmarks={bookmarks} />
+      <BookmarkList bookmarks={bookmarks} onDelete = {deleteBookmark} />
 
     </div>
   );
