2453d77facf61cfe48755b429eedb52c5069c252
Abdulhafiz Muhammed <muhammedabdulhafiz1993@gmail.com>
2025-07-20 22:40:26 -0700

    adding book mark component

--- bookmark-manager/src/components/BookmarkForm.jsx
+++ bookmark-manager/src/components/BookmarkForm.jsx
@@ -1,25 +1,58 @@
-function BookmarkForm() {
+import { useState } from "react";
+function BookmarkForm({onAdd}) {
+
+    const [url, setUrl] = useState("");
+    const [title, setTitle] = useState("");
+    const [note, setNote] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!url || !title) {
+            alert("Please fill in both title and URL.");
+            return;
+        }
+        // Here you would typically send the data to your backend or state management
+    const newBookmark = {
+      id: Date.now(),
+      title,
+      url,
+      note,
+    };
+        console.log("Bookmark added:", { title, url, note });
+        // Reset form fields
+        setTitle("");
+        setUrl("");
+        setNote("");
+    }
   return (
-    <form className="bg-white shadow p-4 rounded space-y-4 mb-6">
+    <form onSubmit={handleSubmit} className="bg-white shadow p-4 rounded space-y-4 mb-6">
       <div>
       <label className="block font-medium text-sm mb-1">Title</label>
       <input
         type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
         placeholder="Bookmark Title"
         className="border p-2 mr-2"
+        required
       />
       </div>
       <div>
          <label className="block font-medium text-sm mb-1">URL</label>
         <input
         type="url"
+        value={url}
+        onChange={(e) => setUrl(e.target.value)}
         placeholder="Bookmark URL"
         className="border p-2 mr-2"
+        required
       />
       </div>
             <div>
         <label className="block font-medium text-sm mb-1">Note (optional)</label>
         <textarea
+            value={note}
+            onChange={(e) => setNote(e.target.value)}
           className="w-full border rounded p-2"
           placeholder="Why is this useful?"
         />
--- bookmark-manager/src/pages/Home.jsx
+++ bookmark-manager/src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import BookmarkForm from "../components/BookmarkForm";
+import { useState } from "react";
+
 function Home() {
+const [bookmarks, setBookmarks] = useState([]);
+const addBookmark = (bookmark) => {
+    setBookmarks([bookmark ,...bookmarks]);
+};    
   return (
     <div>
       <h2 className="text-2xl font-bold mb-4">Your Bookmarks</h2>
       {/* BookmarkForm and BookmarkList will go here */}
-      < BookmarkForm />
+      < BookmarkForm onAdd = {addBookmark} />
     </div>
   );
 }
