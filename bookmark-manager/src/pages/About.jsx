a6ef4d4e951648a835f33a7273997c459540aaeb
Abdulhafiz Muhammed <muhammedabdulhafiz1993@gmail.com>
2025-07-20 21:26:19 -0700

    intialing bookmanger app

---
+++ bookmark-manager/src/pages/About.jsx
@@ -0,0 +1,9 @@
+function About() {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">About</h2>
+      <p>This app helps you save and manage your favorite web bookmarks.</p>
+    </div>
+  );
+}
+export default About;
---
+++ bookmark-manager/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold mb-4">Your Bookmarks</h2>
+      {/* BookmarkForm and BookmarkList will go here */}
+    </div>
+  );
+}
+
+export default Home;
