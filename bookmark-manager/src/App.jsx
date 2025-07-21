fe6de94a4749fd08cd7633d3c5ed3674dbb21d32
Abdulhafiz Muhammed <muhammedabdulhafiz1993@gmail.com>
2025-07-20 21:31:37 -0700

    basic ui is developed

--- bookmark-manager/src/App.jsx
+++ bookmark-manager/src/App.jsx
@@ -1,10 +1,27 @@
-
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
 
 function App() {
 
   return (
     <>
-    
+      <BrowserRouter>
+      <header className="bg-white shadow px-4 py-3 flex justify-between items-center">
+        <h1 className="text-xl font-bold">📑 Bookmark Manager</h1>
+        <nav className="space-x-4">
+          <Link className="text-blue-600 font-medium" to="/">Home</Link>
+          <Link className="text-blue-600 font-medium" to="/about">About</Link>
+        </nav>
+      </header> 
+           <main className="p-4 max-w-3xl mx-auto">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+
     </>
   )
 }
